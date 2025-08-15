906d67fb1a1d33bc930ca74890e01f25c69e5ae2
ASHIQ02KHAN <ashiq2khan02@gmail.com>
2025-08-15 19:22:52 +0600

    Basic input,output

---
+++ Basic Python/print.py
@@ -0,0 +1,12 @@
+# Basic print statement
+print("Hello, World!")  # Prints text to the screen
+
+# Printing multiple items
+print("My name is", "Khan")  # Comma separates multiple values
+
+# f-string formatting (new & preferred way)
+name = "Khan"
+print(f"My name is {name}")  # Inserts variable into string
+
+# Using old format method
+print("My name is {}".format(name))  # Works but f-string is simpler
