1851b7bcc1c8fa30709d51520d01655d856739be
ASHIQ02KHAN <ashiq2khan02@gmail.com>
2025-08-16 21:03:48 +0600

    Using of Operators

---
+++ Basic Python/operators.py
@@ -0,0 +1,21 @@
+# Arithmetic operators
+a, b = 10, 3
+print(a + b)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Multiplication
+print(a / b)  # Division (float)
+print(a // b) # Floor division
+print(a % b)  # Modulus (remainder)
+print(a ** b) # Power (10^3 = 1000)
+
+# Comparison operators
+print(a == b)  # Equal?
+print(a != b)  # Not equal?
+print(a > b)   # Greater than?
+print(a < b)   # Less than?
+
+# Logical operators
+x, y = True, False
+print(x and y)  # AND
+print(x or y)   # OR
+print(not x)    # NOT
