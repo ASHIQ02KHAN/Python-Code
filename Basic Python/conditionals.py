22e0ebe7bcbe665a075ca3b072eeb77c4e4e7af5
ASHIQ02KHAN <ashiq2khan02@gmail.com>
2025-08-16 21:06:21 +0600

    Conditional statements

---
+++ Basic Python/conditionals.py
@@ -0,0 +1,22 @@
+# if statement
+age = 18
+if age >= 18:
+    print("You are an adult")  # Runs if condition is True
+
+# if-else
+age = 16
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are underage")
+
+# if-elif-else
+marks = 85
+if marks >= 90:
+    print("Grade A")
+elif marks >= 80:
+    print("Grade B")
+elif marks >= 70:
+    print("Grade C")
+else:
+    print("Grade D")
