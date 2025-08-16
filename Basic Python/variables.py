3f9e1e99e3827b37a99fa200dde5e78cbddde6a0
ASHIQ02KHAN <ashiq2khan02@gmail.com>
2025-08-16 21:00:55 +0600

    Variables and Data Types

---
+++ Basic Python/variables.py
@@ -0,0 +1,23 @@
+# Integer
+age = 20  
+print(type(age))  # Shows data type -> <class 'int'>
+
+# Float
+price = 99.99
+print(type(price))  # <class 'float'>
+
+# String
+name = "Python"
+print(type(name))  # <class 'str'>
+
+# Boolean
+is_student = True
+print(type(is_student))  # <class 'bool'>
+
+# Multiple variable assignment
+x, y, z = 10, 20, 30
+print(x, y, z)
+
+# Assign same value to multiple variables
+a = b = c = 5
+print(a, b, c)
